1f42b0741e920064a11a9f027a4340c72a937fd0
akilaisuru <akilaisurukahingala@gmail.com>
2026-07-04 23:33:02 +0530

    add leave request form

--- lib/leave_page.dart
+++ lib/leave_page.dart
@@ -1,3 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class LeavePage extends StatefulWidget {
@@ -8,13 +11,126 @@ class LeavePage extends StatefulWidget {
 }
 
 class _LeavePageState extends State<LeavePage> {
+  final TextEditingController _reasonController = TextEditingController();
+  DateTime? _selectedDate;
+
+  Future<void> _pickDate() async {
+    DateTime? pickedDate = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime.now(),
+      lastDate: DateTime(2030),
+    );
+    if (pickedDate != null) {
+      setState(() {
+        _selectedDate = pickedDate;
+      });
+    }
+  }
+
+  Future<void> _submitLeaveRequest() async {
+    if (_reasonController.text.isEmpty || _selectedDate == null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("please enter a reason and select a date!"),
+          backgroundColor: Colors.redAccent,
+        ),
+      );
+      return;
+    }
+    try {
+      final user = FirebaseAuth.instance.currentUser;
+
+      if (user != null) {
+        await FirebaseFirestore.instance.collection("leave_requests").add({
+          'uid': user.uid,
+          'emial': user.email,
+          'reason': _reasonController.text,
+          'date': _selectedDate,
+          'status': 'pending',
+          'createdAt': FieldValue.serverTimestamp(),
+        });
+
+        setState(() {
+          _reasonController.clear();
+          _selectedDate = null;
+        });
+        if (mounted) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text("Leave Request Submitted Successfully!"),
+              backgroundColor: Colors.green,
+            ),
+          );
+        }
+      }
+    } catch (e) {
+      if (mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text("Error"), backgroundColor: Colors.redAccent),
+        );
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Leave Requests Page\n(Coming Soon...)',
-        textAlign: TextAlign.center,
-        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Apply Leave"),
+        backgroundColor: Colors.blueAccent,
+        foregroundColor: Colors.white,
+      ),
+      body: Padding(
+        padding: const EdgeInsetsGeometry.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Text(
+              "Request a Leave",
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+
+            TextField(
+              controller: _reasonController,
+              decoration: const InputDecoration(
+                labelText: "Reason for leave",
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.edit_document),
+              ),
+            ),
+            const SizedBox(height: 20),
+
+            OutlinedButton.icon(
+              onPressed: _pickDate,
+              icon: const Icon(Icons.calendar_month),
+              label: Text(
+                _selectedDate == null
+                    ? "Select Date"
+                    : "Selected: ${_selectedDate!.toLocal()}".split(' ')[0],
+                style: const TextStyle(fontSize: 16),
+              ),
+              style: OutlinedButton.styleFrom(
+                padding: const EdgeInsets.symmetric(vertical: 15),
+              ),
+            ),
+            const SizedBox(height: 30),
+
+            ElevatedButton(
+              onPressed: _submitLeaveRequest,
+              style: ElevatedButton.styleFrom(
+                // padding: const EdgeInsets.symmetric(vertical: 15),
+                // backgroundColor: Colors.orangeAccent,
+                // foregroundColor: Colors.white,
+              ),
+              child: const Text(
+                "Submit Request",
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
